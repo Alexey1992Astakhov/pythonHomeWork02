34cb461531f24e27be676c94e4b3ff6cf518467d
Alexei <kLaSs1k1806@gmail.com>
2022-10-01 01:06:57 +0300

    Initial commit

---
+++ task_5.py
@@ -0,0 +1,22 @@
+#Реализуйте алгоритм перемешивания списка. Без функции shuffle из модуля random.
+#-> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#-> [0, 7, 6, 3, 4, 2, 9, 5, 1, 8]
+
+from time import time
+
+lister = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+def mix_list(old: list) -> list:
+    new = []
+    while old:
+        x = str(time()).split('.')[1]
+        x = list(map(int, [x[0], x[-1]]))
+        x = x[0] if x[0] <= x[1] else x[1]
+        if x > len(old)-1:
+            new.append(old.pop(0))
+        else:
+            new.append(old.pop(x))
+    return new
+
+lister = mix_list(lister)
+print(lister)
